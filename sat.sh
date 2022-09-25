e814b9fdf7a887cda5168187f510a383e8f57ef2
Vladimir Jerotić <vladimir.jerotic@outlook.com>
2022-09-25 15:17:09 +0200

    Sep. 25. 2022

--- sat.sh
+++ sat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-setsid -f xclock -digital -brief -face 'Bitstream Vera Sans:size=30'
-bash -c 'sleep 1 && echo'
+setsid -f xclock -digital -bg pink -brief -face 'Bitstream Vera Sans:size=39'
+bash -c 'sleep 5 && echo'
 wmctrl -r xclock -b toggle,above
-wmctrl -r xclock -e 1000,4000,140,160,75
+wmctrl -r xclock -e 1000,4000,150,160,75
 wmctrl -x -r xclock.XClock -b add,skip_taskbar
+wmctrl -x -r xclock.XClock -b add,skip_pager
