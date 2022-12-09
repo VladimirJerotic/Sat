f494a129ac08ed0f1fc8b025c35d258025d8d8d4
VladimirJerotic <vladimir.jerotic@outlook.com>
2022-12-09 16:21:35 +0100

    1 sec

--- sat.sh
+++ sat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 setsid -f xclock -digital -bg pink -brief -face 'Bitstream Vera Sans:size=39'
-bash -c 'sleep 5 && echo'
+bash -c 'sleep 1 && echo'
 wmctrl -r xclock -b toggle,above
 wmctrl -r xclock -e 1000,4000,150,160,75
 wmctrl -x -r xclock.XClock -b add,skip_taskbar
